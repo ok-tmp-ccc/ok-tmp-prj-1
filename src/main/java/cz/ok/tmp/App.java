cc8710f48ecf239f93fe7506355d2feaad6bd182
ok-tmp-ccc <ok.tmp.ccc@gmail.com>
2011-09-07 11:51:13 +0300

    code review 2

--- src/main/java/cz/ok/tmp/App.java
+++ src/main/java/cz/ok/tmp/App.java
@@ -3,7 +3,7 @@ package cz.ok.tmp;
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("Hello GitHub !!!");
+    System.out.println("Hello GitHub !!! ???");
   }
 
 }
